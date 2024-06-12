79cb1460616a39f251b0656df86591c19d678aa7
Yop <yopiraeka2@gmail.com>
2024-06-12 20:58:48 +0800

    Commit ver1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,16 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
 }
